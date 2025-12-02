a8071f816d1c1cd86efcf2cb29d71da718420c96
AsabCLS <106596823+AsabCLS@users.noreply.github.com>
2025-12-01 21:16:40 -0300

    Alteração de Mensagem de Boas Vindas

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oi gostoso, te amo viu!</h1>
+    return <h1>Olá, este é o primeiro rep criado no intuito de explorar o github e suas ferramentas, incluíndo o terminal git</h1>
 }
 
 export default Home;
